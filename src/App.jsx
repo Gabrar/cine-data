34bad6e75e9e2efb5433d5e9fe300bd005dce706
Gabrar <gabriel2025diasss@gmail.com>
2025-09-03 11:00:39 -0300

    Update to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from './App.module.css'
 
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { HashRouter as Router, Route, Routes} from 'react-router-dom'
 
 import NavBar from './components/layout/NavBar';
 
